2cfb3d0ccb582dabaed6ea660ba05993f5f69165
chanesarcon <sarconchane@gmail.com>
2025-08-27 12:57:39 +0800

    firebase Config

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA9tA_iZ7sLecoLDMB3LaH0KicV3gbJjyo",
-  authDomain: "sarcon-goals.firebaseapp.com",
-  projectId: "sarcon-goals",
-  storageBucket: "sarcon-goals.firebasestorage.app",
-  messagingSenderId: "95708529740",
-  appId: "1:95708529740:web:b0b9ab6353fd1d452c20e9",
-  measurementId: "G-PN7WY1X3T6"
-};
+// Optionally import the services that you want to use
+// import {...} from 'firebase/auth';
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+// import {...} from 'firebase/storage';
 
 // Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
